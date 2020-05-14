86c7e3d711e407810192b4cc39194be6a8ca089d
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-14 03:21:35 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,6 +1,8 @@
 def find_min_value(array)
   # Add your solution here
+  min = 0
+  for number in array do
+    min = min >= number ? number : min
+  end
+  return min
 end
-
-a = min { |2, 3|  }
-puts a
